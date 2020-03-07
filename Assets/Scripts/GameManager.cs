605d8b5cde3a1f502708d6d368fcc26ba28cbb97
SvenAhlgrimm <SvenAhlgrimm@users.noreply.github.com>
2020-03-07 12:22:20 +0100

    Line Mechanic

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    bool lineIsGenerating;
+    public Line linePrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("space"))
+        {
+            if(!lineIsGenerating)
+            {
+                StartNewline();
+            }
+        }       
+    }
+
+    public void StartNewline()
+    {
+        Instantiate(linePrefab);
+        lineIsGenerating = true;
+    }
+
+    public void OnLineFinished()
+    {
+        lineIsGenerating = false;
+    }
+}
---
+++ Assets/Scripts/Line.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Line : MonoBehaviour
+{
+    float percentage, heightPercentage, addedHeight;
+    public float speed, heightInfluence, xScale,yScale;
+    LineRenderer lineRenderer;
+    GameManager gameManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        lineRenderer = GetComponent<LineRenderer>();
+
+         StartCoroutine(Generate());
+    }
+
+    IEnumerator Generate()
+    {
+        while (percentage < 1)
+        {
+
+            if (Input.GetKey("space"))
+            {
+                addedHeight = Mathf.Lerp(addedHeight, heightInfluence, Time.deltaTime);
+            }
+            else
+            {
+                addedHeight = Mathf.Lerp(addedHeight, -heightInfluence, Time.deltaTime);
+                //heightPercentage -= Mathf.Lerp(heightPercentage, heightInfluence, Time.deltaTime); //+= heightInfluence * Time.deltaTime;
+
+                //heightPercentage -= heightInfluence * Time.deltaTime;
+
+            }
+            heightPercentage += addedHeight;
+
+            percentage += Time.deltaTime * speed;
+            lineRenderer.positionCount++;
+            Vector3 newPointPosition = new Vector3(-xScale / 2 + xScale * percentage, -yScale / 2 + yScale * heightPercentage);
+            lineRenderer.SetPosition(lineRenderer.positionCount - 1, newPointPosition);
+            yield return new WaitForEndOfFrame();
+        }
+        gameManager.OnLineFinished();
+                    
+    }
+}
